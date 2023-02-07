0f4303c00f43ccf95159f54e6ba3007a0dfb29c4
Wedi <84606803+wedi0@users.noreply.github.com>
2023-02-07 18:48:55 -0300

    Update Banco.java

--- Banco/src/Banco.java
+++ Banco/src/Banco.java
@@ -24,7 +24,7 @@ public class Banco implements IBanco{
     @Override
     public void consultarExtrato(Conta conta) {
         
-        conta.consultarExtrato(conta);
+        conta.consultarExtrato();
         
     }
 
